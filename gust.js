d8083eab35ebade87365dc2e40b2dbbfcf58a7c5
Grant James <grant.james@cardinalhealth.com>
2016-11-01 17:55:37 -0400

    GUST-1469 wip

--- gust.js
+++ gust.js
@@ -9,8 +9,39 @@ Array.prototype.getUnique = function(){
    }
    return a;
 }
+var colors = require('colors');
+colors.setTheme({
+      silly: 'rainbow',
+      input: 'grey',
+      verbose: 'cyan',
+      prompt: 'grey',
+      good: 'green',
+      staged: 'green',
+      data: 'grey',
+      help: 'cyan',
+      warn: 'yellow',
+      debug: 'blue',
+      important: 'blue',
+      change: 'red',
+      RED: 'red',
+      error: 'red',
+      name: 'red',
+      logo: 'green',
+      title: 'green',
+      arg: 'white'
+    });
 
+var cliArgs = process.argv.slice(2);
+if(!cliArgs[0]){
+    console.log("\n--- ---\n".error,
+    "Not root dir specified.  Using environment variable: 'GUST_ROOT'".error,
+    ("\n[" + process.env.GUST_ROOT + "]").warn,
+    "\n--- ---\n".error);
+}
+var rootDir = cliArgs[0] || process.env.GUST_ROOT;
 var fileDescriber = new require(__dirname + "/lib/file_describer.js")();
+var fileDescriptions = fileDescriber.findAndDescribeJavaFiles(rootDir);
+console.log(("Found " + fileDescriptions.length + " files").important);
 
-console.log(fileDescriber.findAndDescribeJavaFiles("/Users/grant.james/projects/_alfred/app/src/main"));
+//console.log(d);
 
