ebf02fd8739471f8ca70289fcbc2bb8b03edf455
Grant James <grant.james@cardinalhealth.com>
2016-11-01 17:25:03 -0400

    GUST-1469 wip

--- gust.js
+++ gust.js
@@ -1,5 +1,16 @@
+Array.prototype.getUnique = function(){
+   var u = {}, a = [];
+   for(var i = 0, l = this.length; i < l; ++i){
+      if(u.hasOwnProperty(this[i])) {
+         continue;
+      }
+      a.push(this[i]);
+      u[this[i]] = 1;
+   }
+   return a;
+}
 
-var fileFinder = new require(__dirname + "/lib/file_finder.js")();
+var fileDescriber = new require(__dirname + "/lib/file_describer.js")();
 
-console.log(fileFinder.findAllJavaFiles("/Users/grant.james/projects/_alfred/app/src/main").length);
+console.log(fileDescriber.findAndDescribeJavaFiles("/Users/grant.james/projects/_alfred/app/src/main"));
 
